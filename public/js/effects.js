277f0c63b43d9771f07b282425b9fbb05191b16c
Agnija <ilzena.agnija@gmail.com>
2022-06-21 10:55:10 +0200

    js effects, home css

--- public/js/effects.js
+++ public/js/effects.js
@@ -1,30 +1,22 @@
-// let options = {
-//     root: document.querySelector('#scrollArea'),
-//     rootMargin: '0px',
-//     threshold: 1.0
-//   }
-  
-//   let observer = new IntersectionObserver(callback, options);
+// Delay effects in HOME page
 const ratio = .1;
 
-  let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: ratio
-  }
-  
-  const handleIntersect = function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.intersectionRatio > ratio) {
-                entry.target.classList.add('reveal-visible')
-                observer.unobserve(entry.target)
-            } 
-       
-    })
-   
-  }
+let options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+}
 
-  const observer = new IntersectionObserver(handleIntersect, options);
-  document.querySelectorAll('.reveal').forEach(function(r) {
-    observer.observe(r);
-  })
+const handleIntersect = function(entries, observer) {
+      entries.forEach(function(entry) {
+          if (entry.intersectionRatio > ratio) {
+              entry.target.classList.add('reveal-visible')
+              observer.unobserve(entry.target)
+          }        
+  })   
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function(r) {
+  observer.observe(r);
+})
