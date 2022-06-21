dbf26428c9d51b192a0a3a9e9669dcc032fa5942
Agnija <ilzena.agnija@gmail.com>
2022-06-21 16:59:12 +0200

    stage

--- public/js/effects.js
+++ public/js/effects.js
@@ -20,3 +20,42 @@ const observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll('.reveal').forEach(function(r) {
   observer.observe(r);
 })
+
+// Modal
+const btnDetails = document.querySelectorAll('.btnDetails');
+btnDetails.forEach(btn => {
+
+        // Get the modal
+        let modal = document.getElementById("myModal");
+        // Get the <span> element that closes the modal
+        let span = document.getElementsByClassName("close")[0];
+
+        let el = document.getElementById("showName");
+
+        btn.addEventListener('click', (event)=> {
+          event.preventDefault();
+          modal.style.display = "block";
+          // Insert Data here
+          console.log(el.dataset.massage.name);
+        })
+
+        span.addEventListener('click', (event)=> {
+          event.preventDefault();
+          modal.style.display = "none";
+        })
+        
+        window.addEventListener('click', (event)=> {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        })
+
+        console.log('hello');
+        // console.log(el.dataset.name);
+
+        
+})
+
+
+
+
