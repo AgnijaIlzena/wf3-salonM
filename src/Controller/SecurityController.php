670a4a3c1ace191348a8dc3eabb834819342727d
Alexandre DA SILVA <alexandre71100@hotmail.com>
2022-06-22 14:41:33 +0200

    hash password lister

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,16 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //     return $this->redirectToRoute('admin');
         // }
 
+        
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -1,33 +1,56 @@
 <?php
 
-namespace App\Controller\EventSubscriber;
+namespace App\EventSubscriber;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class EasyAdminSubscriber implements EventSubscriberInterface {
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+	private UserPasswordHasherInterface $security;
 
-    private $security;
-    
-    public function  __construct(UserPasswordHasherInterface $passwordHasher) {
-        $this->security = $passwordHasher;
-    }
- 
-    public static function getSubscribedEvents(){
-        return [
-            BeforeEntityPersistedEvent::class => ["setHashPassword"],
-        ];
-    }
+	public function  __construct(UserPasswordHasherInterface $passwordHasher)
+	{
+		$this->security = $passwordHasher;
+	}
 
-    public function setHashPassword(BeforeEntityPersistedEvent $event){
-        $entity = $event->getEntityInstance();
-        if(!$entity instanceof User){
-            return ;
-        }
+	public static function getSubscribedEvents()
+	{
+		return [
+			BeforeEntityPersistedEvent::class => ['createHashPassword'],
+			BeforeEntityUpdatedEvent::class => ['updateHashPassword'],
+		];
+	}
 
-        $entity->setPassword($this->security->hashPassword($entity, $entity->getPassword()));
-    
-    }
+	public function createHashPassword(BeforeEntityPersistedEvent $event)
+	{
+		$entity = $event->getEntityInstance();
+
+		if (!$entity instanceof User) {
+			return;
+		}
+
+		$this->setHashPassword($entity);
+	}
+
+	public function updateHashPassword(BeforeEntityUpdatedEvent $event)
+	{
+		$entity = $event->getEntityInstance();
+
+		if (!$entity instanceof User) {
+			return;
+		}
+
+		$this->setHashPassword($entity);
+	}
+
+	public function setHashPassword(User $user)
+	{
+		$user->setPassword(
+			$this->security->hashPassword($user, $user->getPassword())
+		);
+	}
 }
